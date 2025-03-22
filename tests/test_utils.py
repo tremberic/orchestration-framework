e0999a46f313c39af82da67678db6629f25c18ad
Alejandro Herrera <149527975+sfc-gh-alherrera@users.noreply.github.com>
2025-03-22 12:16:21 -0400

    Revert "feat: switch over search and complete to use core API (#126)"
    This reverts commit 7c2b5c2e4bd2243ed89c7eb87dbe340a7739866b.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -33,6 +33,26 @@ def session():
     return MockConnection(host="example_host", token="dummy_token")
 
 
+def test_complete_url(session):
+    eb = CortexEndpointBuilder(session)
+    url = eb.get_complete_endpoint()
+    assert url.startswith("https://")
+    assert url.endswith("/api/v2/cortex/inference:complete")
+    assert "_" not in urlparse(url).hostname
+
+
+def test_search_url(session):
+    eb = CortexEndpointBuilder(session)
+    url = eb.get_search_endpoint(
+        database="TEST_DB", schema="TEST_SCHEMA", service_name="TEST_SERVICE"
+    )
+    assert url.startswith("https://")
+    assert url.endswith(
+        "/api/v2/databases/test_db/schemas/test_schema/cortex-search-services/test_service:query"
+    )
+    assert "_" not in urlparse(url).hostname
+
+
 def test_analyst_url(session):
     eb = CortexEndpointBuilder(session)
     url = eb.get_analyst_endpoint()
@@ -41,8 +61,24 @@ def test_analyst_url(session):
     assert "_" not in urlparse(url).hostname
 
 
+def test_complete_headers(session):
+    eb = CortexEndpointBuilder(session)
+    headers = eb.get_complete_headers()
+    assert headers["Content-Type"] == "application/json"
+    assert headers["Authorization"] == 'Snowflake Token="dummy_token"'
+    assert headers["Accept"] == "application/json"
+
+
 def test_analyst_headers(session):
     eb = CortexEndpointBuilder(session)
     headers = eb.get_analyst_headers()
     assert headers["Content-Type"] == "application/json"
     assert headers["Authorization"] == 'Snowflake Token="dummy_token"'
+
+
+def test_search_headers(session):
+    eb = CortexEndpointBuilder(session)
+    headers = eb.get_search_headers()
+    assert headers["Content-Type"] == "application/json"
+    assert headers["Authorization"] == 'Snowflake Token="dummy_token"'
+    assert headers["Accept"] == "application/json"
